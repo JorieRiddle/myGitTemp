c9d2dea0fcbc6eb5ad72e27c4821e89bff4eeaab
JorieRiddle <74027655+JorieRiddle@users.noreply.github.com>
2020-11-09 17:09:47 +0800

    Update 水仙花数.cpp

--- 水仙花数.cpp
+++ 水仙花数.cpp
@@ -5,17 +5,17 @@ int main()
 	scanf("%d",&x);
 	a=x/100;
     b=(x-100*a)/10;
-    c=x%10;
+    c=x-100*a-10*b;
     if(x>99 && x<1000)
 	{
 	
-        if(x==a^3+b^3+c^3)
+       if(x==a*a*a+b*b*b+c*c*c)
 		{
 			printf("%d=%d*%d*%d+%d*%d*%d+%d*%d*%d\n",x,a,a,a,b,b,b,c,c,c);
 		}
 		else
 		{
-			printf("不是水仙花数\n");
+			printf("Not a narcissistic number\n");
 		}    
 	}
 	else
