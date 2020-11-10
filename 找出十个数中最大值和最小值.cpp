99ee09fbda8cdd46121fcf5a63cca70ebf353aca
JorieRiddle <Ray18952875510@163.com>
2020-11-10 18:11:52 +0800

    输入十个数找出最大值和最小值

---
+++ 找出十个数中最大值和最小值.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+   int a[10],i,max,min;
+   for(i=0;i<10;i++)
+	   scanf("%d",&a[i]);
+   max=min=a[0];
+   for(i=1;i<10;i++)
+   {
+    if(a[i]>a[0])
+	max=a[i];
+       
+	if(a[i]<a[0])
+	min=a[i];
+   }
+   printf("%d,%d",max,min);
+
+
+
+	return 0;
+}
