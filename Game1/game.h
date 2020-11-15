4a0ed5fedf8950b9b0d2bff66b69f7e75e84f415
JorieRiddle <Ray18952875510@163.com>
2020-11-15 19:59:13 +0800

    三子棋

---
+++ Game1/game.h
@@ -0,0 +1,15 @@
+#define ROW 3
+#define COL 3
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+//声明
+void InitBoard(char board[ROW][COL],int row,int col);
+void Displayboard(char board[ROW][COL],int row,int col);
+void PlayerMove(char board[ROW][COL],int row,int col);
+void ComputerMove(char board[ROW][COL],int row,int col);
+
+//告诉我四种状态
+//玩家赢'*'，电脑赢'#'，平局'Q'，继续游戏'C'
+char IsWin(char board[ROW][COL],int row,int col);
