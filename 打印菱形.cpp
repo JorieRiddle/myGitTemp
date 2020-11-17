5c7d44e7c4219e6472c4ab7235ee7595176b90fe
JorieRiddle <Ray18952875510@163.com>
2020-11-17 16:57:06 +0800

    打印菱形

---
+++ 打印菱形.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+int main()
+{
+ int line=6;
+ int i;
+//打印上半部分，包括中间一行 
+ for(i=1;i<=line;i++)
+ {
+	 //打印一行
+	 //打印空格
+	 int j;
+     for(j=1;j<=line-i;j++)
+		 {
+			 printf(" ");
+		 }
+	 //打印*
+	 for(j=0;j<2*(i-1)+1;j++)
+	 {
+		 printf("*");
+	 }
+	 printf("\n");	 
+ }
+ //打印下半部分
+for(i=1;i<=line-1;i++)
+ {
+	 //打印一行
+	 //打印空格
+	 int j=0;
+     for(j=0;j<i;j++)
+		 {
+			 printf(" ");
+		 }
+	 //打印*
+	 for(j=0;j<2*(line-i)-1;j++)
+	 {
+		 printf("*");
+	 }
+	 printf("\n");	 
+ }
+}
