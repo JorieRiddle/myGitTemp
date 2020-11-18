6b7a8f3450a118b136d064f17d7d9004117884df
JorieRiddle <Ray18952875510@163.com>
2020-11-18 19:13:15 +0800

    杨辉三角

---
+++ 杨辉三角.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#define M 10
+#define N 10
+int main()
+{   
+	int i,j;
+	int arr[M][N]={0};
+	for(i=0;i<M;i++)
+	{
+		arr[i][0]=arr[i][i]=1;  
+		//不加{}
+	}
+    for(i=2;i<M;i++)
+	{
+		for(j=1;j<i;j++)
+		{
+		   arr[i][j]=arr[i-1][j]+arr[i-1][j-1];   //要另外给for，因为i从2开始
+		}
+	}
+	for(i=0;i<M;i++)
+	{		
+		for(j=0;j<=i;j++)
+		{
+
+		   printf("%4d",arr[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
