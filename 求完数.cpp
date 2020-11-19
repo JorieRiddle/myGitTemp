c38413a927a68c143189fc0edd45c927aafc0a55
JorieRiddle <Ray18952875510@163.com>
2020-11-19 17:02:27 +0800

    求完数

---
+++ 求完数.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+  int m,s,i;
+  for(m=2;m<1000;m++)
+  {
+      s=0;   //s只能放这里
+	  for(i=1;i<m;i++)
+	  {
+		 
+		  if((m%i)==0)
+		  {
+			  s=s+i;
+		  }
+	  }
+	  if(m==s)
+	  {
+		  printf("%d\n",s);
+	  }
+  }
+
+  return 0;
+}
