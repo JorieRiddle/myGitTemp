60c67668d23014df675ca0f073fce5ceb00335c5
JorieRiddle <Ray18952875510@163.com>
2020-11-19 17:01:12 +0800

    最小公倍数和最大公约数

---
+++ 最小公倍数和最大公约数.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+int m,n,a,b;
+scanf("%d%d",&m,&n);
+if(m==0 || n==0)
+a=0,b=1;
+else
+{
+	for(a=m;;a++)
+	{
+		if(a%m==0 && a%n==0)
+			break;
+	}
+}
+for(b=n;;b--)
+{
+	if(m%b==0 && n%b==0)
+		break;
+}
+printf("%d,%d",a,b);
+return 0;
+}
