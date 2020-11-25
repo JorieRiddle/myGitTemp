d658a826937b7153ebd4365b7f567831eeab167a
JorieRiddle <Ray18952875510@163.com>
2020-11-25 20:35:08 +0800

    插入一个数

---
+++ 插入一个数.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main()
+{   
+	int a[8]={2,5,7,10,14,17,20};
+	int i,j,n,end;
+	scanf("%d",&n);
+	end=a[6];
+	if(n>end)
+	{
+		a[7]=n;
+	}
+	else
+	{
+		for(i=0;i<7;i++)
+		{
+			if(a[i]>n)
+			{
+				for(j=7;j>i;j--)           //不能用i++，因为前面的数会把后面遮住
+				{
+					a[j]=a[j-1];
+				}
+				a[i]=n;
+				break;
+			}
+		}
+	}
+	for(i=0;i<8;i++)
+		printf("%d\t",a[i]);
+	printf("\n");
+	return 0;
+}
